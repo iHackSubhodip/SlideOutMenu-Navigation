0627e7385dae8b4467db01065d13ca1623cad393
Subhodip <differentialthinking@gmail.com>
2018-12-25 14:44:07 +0530

    1st commit.

---
+++ SlideOutMenu/SlideOutMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Banerjee,Subhodip on 25/12/18.
+//  Copyright © 2018 Banerjee,Subhodip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
