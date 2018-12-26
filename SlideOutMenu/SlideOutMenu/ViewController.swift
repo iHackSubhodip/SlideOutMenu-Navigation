21dd1ccbffe80dda51506a56517b7465f624372b
Subhodip <differentialthinking@gmail.com>
2018-12-26 12:09:05 +0530

    Setup added.

--- SlideOutMenu/SlideOutMenu/ViewController.swift
+++ SlideOutMenu/SlideOutMenu/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupNavigationItems()
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(handleClose))
     }
 
-
+    @objc func handleOpen(){
+        print("Open")
+    }
+    
+    @objc func handleClose(){
+        print("Close")
+    }
+    
 }
 
+//MARK: TableView DataSource
+
+extension ViewController{
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        cell.textLabel?.text = "Yo : \(indexPath.row)"
+        return cell
+    }
+}
