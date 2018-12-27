1e64712ca17075c815b693aad427adaab984d4f7
Subhodip <differentialthinking@gmail.com>
2018-12-27 15:49:18 +0530

    Pan gesture added.

--- SlideOutMenu/SlideOutMenu/Controller/HomeController.swift
+++ SlideOutMenu/SlideOutMenu/Controller/HomeController.swift
@@ -16,28 +16,58 @@ class HomeController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationItems()
+        setupMenuController()
+        setupGestureRecognizer()
     }
+    
+
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Home"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(handleClose))
     }
-
-    @objc func handleOpen(){
+    
+    fileprivate func setupMenuController() {
         menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
         let window = UIApplication.shared.keyWindow
         window?.addSubview(menuController.view)
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.menuController.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
-        })
         addChild(menuController)
     }
     
-    @objc func handleClose(){
+    fileprivate func performAnimations(transform: CGAffineTransform) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.menuController.view.transform = .identity
+            self.menuController.view.transform = transform
+            self.navigationController?.view.transform = transform
         })
+    }
+    
+    fileprivate func setupGestureRecognizer() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer){
+        let translation = gesture.translation(in: view)
+        if gesture.state == .changed{
+            var x = translation.x
+            x = min(menuWidth, x)
+            x = max(0, x)
+            let transform = CGAffineTransform(translationX: translation.x, y: 0)
+            self.menuController.view.transform = transform
+            self.navigationController?.view.transform = transform
+        }else{
+            handleOpen()
+        }
+        
+    }
+    
+    @objc func handleOpen(){
+        performAnimations(transform: CGAffineTransform(translationX: menuWidth, y: 0))
+    }
+    
+    @objc func handleClose(){
+        performAnimations(transform: .identity)
     }
     
 }
